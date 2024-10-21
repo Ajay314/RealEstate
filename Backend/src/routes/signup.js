6dbe1d6ce9a9c6fc0e42c2856d58394cef489519
Ajay314 <109684456+Ajay314@users.noreply.github.com>
2024-10-21 23:47:45 +0530

    Created Login Backend Api

--- Backend/src/routes/signup.js
+++ Backend/src/routes/signup.js
@@ -8,24 +8,50 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 
-router.post("/signup", async (req,res) =>{
+router.post("/signup", async (req, res) => {
 
     try {
         validateSignUpData(req);
-        const { name , email, password } = req.body;
-       const passwordHash = await bcrypt.hash(password,10);
-   
+        const { name, email, password } = req.body;
+        const passwordHash = await bcrypt.hash(password, 10);
+
         const user = new Signup({
-           name,
-           email,  
-           password:passwordHash
+            name,
+            email,
+            password: passwordHash
         });
         await user.save();
         res.send("User added");
-      }
-      catch (err) {
-       res.status(400).send("ERROR:" + err.message);
-      }
+    }
+    catch (err) {
+        res.status(400).send("ERROR:" + err.message);
+    }
+})
+
+router.post("/login", async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+        const user = await Signup.findOne({ email: email });
+        if (!user) {
+            throw new Error("User Not Found");
+        }
+
+        const isPasswordValid = await Signup.validatePassword(password);
+        if (!isPasswordValid) {
+            throw new Error("Password is not Correct");
+        }
+        else {
+
+            const token = await user.getJWT();
+            res.cookie("token", token);
+            res.send("Login Sucess");
+        }
+    }
+    catch (err) {
+        res.status(400).send("ERROR:" + err.message);
+    }
 })
 
 module.exports = router;
